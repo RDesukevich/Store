5b3683737ccc7090d6918026740864d9f4e6836d
RDESUKEVICH\91410 <9141045@gmail.com>
2022-01-06 15:41:25 +0300

    Fix

--- Store/Models/ApplicationType.cs
+++ Store/Models/ApplicationType.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Store.Models
 {
     public class ApplicationType
     {
         public Guid Id { get; set; }
+        [Required]
         public string Name { get; set; }
     }
 }
